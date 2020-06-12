3c5c3ddc71111886fa0eec8d0668a64aef6aac60
Michael Wender <michael@michaelwender.com>
2020-06-12 06:00:01 -0400

    First commit

---
+++ acf-woocommerce-automation.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     ACF WooCommerce Automation
+ * Plugin URI:      https://github.com
+ * Description:     Automate tasks in association with WooCommerce orders. Requires Advanced Custom Fields Pro.
+ * Author:          TheWebist
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     acf-woocommerce-automation
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Acf_Woocommerce_Automation
+ */
+
+// Your code starts here.
