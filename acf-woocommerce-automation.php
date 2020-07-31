89939c8a527491bc7e4813b929cbcc4754af68d9
Michael Wender <michael@michaelwender.com>
2020-07-31 10:20:36 -0400

    Documenting 1.1.0

--- acf-woocommerce-automation.php
+++ acf-woocommerce-automation.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     acf-woocommerce-automation
  * Domain Path:     /languages
- * Version:         1.0.3
+ * Version:         1.1.0
  *
  * @package         Acf_Woocommerce_Automation
  */
